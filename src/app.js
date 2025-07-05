84d265022b3de20434e1cef49cb020c0c16a0ce3
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-05 07:30:46 -0300

    DEVH: FEAT: Conexao bd mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
 class App {
     constructor() {
+        config();
         this.server = express();
+        mongoose.connect(process.env.MONGODB_URI)
         this.middlewares();
         this.routes();
     }
