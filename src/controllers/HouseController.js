4024613c68c0733c2279082a66202e1ec9ec5673
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-07 09:56:32 -0300

    DEVH: FEAT: Criacao logica cadastro de casas

---
+++ src/controllers/HouseController.js
@@ -0,0 +1,11 @@
+class HouseController {
+
+    async store(req, res) {
+        console.log(req.body);
+        console.log(req.file);
+
+        return res.json({OK: true})
+    }
+}
+
+export default new HouseController();
