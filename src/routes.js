b1d0815cee09dde9a82f7917d31234234706ba44
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-07 09:58:34 -0300

    DEVH: FEAT: Criacao logica multipart form data

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import SessionController from "./controllers/SessionController";
+import HouseController from "./controllers/HouseController";
+import multer from "multer";
+import uploadConfiguration from "./config/upload";
 
 const router = new Router();
+const upload = multer(uploadConfiguration);
 
 router.post("/sessions", SessionController.store);
+
+router.post("/houses", upload.single('thumbnail'), HouseController.store);
 
 export default router;
